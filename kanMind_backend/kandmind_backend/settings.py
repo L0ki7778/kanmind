a9e33feaf1b7766eae923eef6a99feff8bf9c3a4
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2025-04-03 21:39:51 +0200

    added permissionclass and auth token to settings

--- kanMind_backend/kandmind_backend/settings.py
+++ kanMind_backend/kandmind_backend/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'corsheaders',  # add corsheaders to installed apps
     'boards',
     'auth',
-    'tasks'
+    'tasks',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK ={
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES':
+        ['rest_framework.authentication.TokenAuthentication']
+}
