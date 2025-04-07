6b811ac30601d18d60d3a804195609a3f743fe2b
Rene-Heller <rene-heller@developerakademie.com>
2025-04-07 05:17:04 +0200

    added vscode color themes and fixed refresh

--- kanMind_backend/refresh.bat
+++ kanMind_backend/refresh.bat
@@ -1,5 +1,5 @@
 @echo off
-pip install --upgrade pip
+python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
 pyhthon manage.py makemigrations
 python manage.py migrate
--- kanMind_backend/refresh.sh
+++ kanMind_backend/refresh.sh
@@ -1,4 +1,4 @@
-pip install --upgrade pip
+python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
 pyhthon manage.py makemigrations
 python manage.py migrate
