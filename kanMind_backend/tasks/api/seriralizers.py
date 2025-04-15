e91716b0e4d00f0868395ea162fe8ee30591312b
Rene-Heller <rene-heller@developerakademie.com>
2025-04-15 13:43:06 +0200

    added list-create serializer for task model

---
+++ kanMind_backend/tasks/api/seriralizers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from ..models import Task
+from authentication.api.serializers import SimpleUserSerializer
+
+
+class TaskListCreateSerializer(serializers.ModelSerializer):
+    assignee = SimpleUserSerializer(read_only=True) # Serializer get's used to display the needed data only
+    reviewer = SimpleUserSerializer(read_only=True)
+    assignee_id = serializers.PrimaryKeyRelatedField( # need id for corresponding model-relation (PK) and disable required to allow Null
+        queryset=User.objects.all(), source='assignee', write_only=True, required = False)
+    reviewer_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), source='reviewer', write_only=True, required = False)
+
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'status', 'priority',
+                  'due_date', 'comments_count', 'board', 'assignee', 'reviewer', 'assignee_id',
+                  'reviewer_id']
