ac6034613dde85f9e5122b9aa47fc119175c7d86
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2025-07-09 11:21:05 +0200

    added view and endpoint for singleBoard-management

--- kanMind_backend/boards/api/views.py
+++ kanMind_backend/boards/api/views.py
@@ -1,7 +1,14 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView,GenericAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework.mixins import RetrieveModelMixin,UpdateModelMixin
 from ..models import Board
 from .serializers import BoardSerializer
 
 class BoardListCreateView(ListCreateAPIView):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
+    
+class BoardSingleView(RetrieveUpdateDestroyAPIView):
+    queryset=Board.objects.all()
+    serializer_class = BoardSerializer
+    lookup_field = 'id'
+    
