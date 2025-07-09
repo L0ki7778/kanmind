d8288df204273cef65857dbadbfce49e9fff7ab0
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2025-07-09 11:18:00 +0200

    created views for task-management on corresponding boards

--- kanMind_backend/tasks/api/views.py
+++ kanMind_backend/tasks/api/views.py
@@ -1,8 +1,10 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, GenericAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, GenericAPIView, ListAPIView
 from rest_framework.mixins import UpdateModelMixin, DestroyModelMixin
 from rest_framework.exceptions import NotFound
-from .seriralizers import TaskListCreateSerializer
-from ..models import Task
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from .seriralizers import TaskListCreateSerializer,CommentListCreateSerializer
+from ..models import Task,Comment
 
 
 class TaskListCreateView(ListCreateAPIView):
@@ -10,16 +12,40 @@ class TaskListCreateView(ListCreateAPIView):
     serializer_class = TaskListCreateSerializer
 
 
-class TaskUpdateDeleteView(GenericAPIView, UpdateModelMixin, DestroyModelMixin):
-    queryset = Task.objects.all()
+class TaskListAssignedView(ListAPIView):
     serializer_class = TaskListCreateSerializer
-    lookup_field = 'id'
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+    def get_queryset(self):
+        user: User = self.request.user
+        return Task.objects.filter(assignee=user)
 
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
 
-    def delete(self, request, *args, **kwargs):
+class TaskListReviewerView(ListAPIView):
+    serializer_class = TaskListCreateSerializer
+
+    def get_queryset(self):
+        return Task.objects.filter(reviewer=self.request.user)
+
+
+class TaskUpdateDeleteView(GenericAPIView, DestroyModelMixin):
+    queryset = Task.objects.all()
+    lookup_url_kwarg:str = 'comment_id'
+
+    def get_object(self) -> Comment:
+        task_id : int = int(self.kwargs.get('task_id'))
+        comment_id : int = int(self.kwargs.get('comment_id'))
+        return Comment.objects.get(pk = comment_id, task__id = task_id)
+    
+    def delete(self,request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
+
+class TaskCommentsListCreateView(ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentListCreateSerializer
+    
+    def perform_create(self, serializer:CommentListCreateSerializer):
+        task_id = self.kwargs.get("task_id")
+        task = get_object_or_404(Task, pk = task_id)
+        serializer.save(author=self.request.user, task=task)
+        
