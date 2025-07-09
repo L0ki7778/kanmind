c3fe9d83778fb0e1d5772ea38462629d2b71b15d
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2025-07-09 11:18:26 +0200

    created serializers and endpoints for task-management on corresponding boards

--- kanMind_backend/tasks/api/seriralizers.py
+++ kanMind_backend/tasks/api/seriralizers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from ..models import Task
+from ..models import Task,Comment
 from authentication.api.serializers import SimpleUserSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+def get_user_full_name(user):
+    return f'{user.username} {user.last_name}'.strip()
 
 
 class TaskListCreateSerializer(serializers.ModelSerializer):
@@ -17,7 +23,21 @@ class TaskListCreateSerializer(serializers.ModelSerializer):
         model = Task
         fields = ['id', 'title', 'description', 'status', 'priority',
                   'comments_count', 'board', 'assignee', 'reviewer', 'assignee_id',
-                  'reviewer_id','due_date']
+                  'reviewer_id', 'due_date']
 
 
-# class TaskDetailSerializer(serializers.ModelSerializer):
+class TaskCommentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['comments_count']
+
+
+class CommentListCreateSerializer(serializers.ModelSerializer):
+    
+    author = serializers.SerializerMethodField()
+    class Meta:
+        model = Comment
+        fields = ["id", "created_at", "author", "content"]
+
+    def get_author(self, obj):
+        return get_user_full_name(obj.author)
--- kanMind_backend/tasks/api/urls.py
+++ kanMind_backend/tasks/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import TaskListCreateView,TaskUpdateDeleteView
+from .views import TaskListCreateView,TaskUpdateDeleteView,TaskListAssignedView,TaskListReviewerView,TaskCommentsListCreateView
 
 urlpatterns = [
     path('', TaskListCreateView.as_view()),
-    path('<int:id>/', TaskUpdateDeleteView.as_view(), name='update_task'),
-    path('<int:id>/assigned-to-me/', TaskUpdateDeleteView.as_view(), name='assigned_task'),
-    path('<int:id>/reviewing/', TaskUpdateDeleteView.as_view(), name='to_review_task'),
+    path('<int:task_id>/', TaskUpdateDeleteView.as_view(), name='update_task'),
+    path('<int:task_id>/comments/', TaskCommentsListCreateView.as_view(), name='comments'),
+    path('<int:task_id>/comments/<int:comment_id>/', TaskUpdateDeleteView.as_view(), name='delete_comments'),
+    path('assigned-to-me/', TaskListAssignedView.as_view(), name='assigned_task'),
+    path('reviewing/', TaskListReviewerView.as_view(), name='to_review_task'),
 ]
