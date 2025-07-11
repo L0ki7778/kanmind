ac2866345902720a13504a77ba370c69f4bcf027
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2025-07-11 13:53:17 +0200

    added missing related_name to board-property in Task-model

--- kanMind_backend/tasks/models.py
+++ kanMind_backend/tasks/models.py
@@ -21,7 +21,7 @@ def one_week_from_now():
     
 
 class Task(models.Model):
-    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE ,related_name='tasks')
     title = models.CharField(max_length=40)
     description = models.TextField(max_length=500, default="")
     status = models.CharField(max_length = 20 , choices=STATUS_STATES, default='to-do')
