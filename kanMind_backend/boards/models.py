810504d234ed17c2f44e618ddd9f740da02acf7f
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2025-07-11 13:44:49 +0200

    modified Board-model for better understanding

--- kanMind_backend/boards/models.py
+++ kanMind_backend/boards/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 class Board(models.Model):
     title = models.CharField(max_length=100)
-    members = models.ManyToManyField(User, related_name='boards')
+    members = models.ManyToManyField(User, related_name='board_members')
     ticket_count = models.IntegerField(default=0)
     tasks_to_do_count = models.IntegerField(default=0)
     tasks_high_prio_count = models.IntegerField(default=0)
-    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
